7c1d35d62ad514cd71a47263ca952e663cc80675
Abhinav Kumar <abhinav@abhinav.com>
2023-03-01 16:42:37 +0530

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -18,9 +18,9 @@ Encore
      * ENTRY CONFIG
      *
      * Each entry will result in one JavaScript file (e.g. app.js)
-     * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
+     * and one CSS file (e.g. app.scss) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/app.js')
+    .addEntry('app/js', './assets/app.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
@@ -57,7 +57,8 @@ Encore
     })
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
+    .addStyleEntry("app/css", "./assets/styles/scss/app.scss")
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
