6cbe1fe5d97c4c43ef6a439ae7ed85327f30c46d
Abhinav Kumar <abhinav@abhinav.com>
2023-03-01 16:04:34 +0530

    Base file update

---
+++ src/Controller/BaseController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class BaseController extends AbstractController
+{
+    #[Route(path: '/{routeType}', name: 'app_home', requirements: ['routeType' => 'home|about|contactus'])]
+    public function home(Request $request, $routeType = 'index'): Response
+    {
+
+
+        return $this->render('base/'. $routeType.'.html.twig', [
+            'site_meta_title_name' => $routeType,
+        ]);
+    }
+}
