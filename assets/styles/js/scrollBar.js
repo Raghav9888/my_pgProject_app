679f3411f7cd43be798e4b09222f8da7b84ed2ac
Raghav <121083996+CommonProjects88@users.noreply.github.com>
2023-03-05 13:22:09 +0530

    Update js

---
+++ assets/styles/js/scrollBar.js
@@ -0,0 +1,11 @@
+// this code use for when refresh the page scroll bar on top
+
+history.scrollRestoration = 'manual';
+
+if (history.scrollRestoration) {
+    history.scrollRestoration = 'manual';
+} else {
+    window.onbeforeunload = function () {
+        window.scrollTo(0, 0);
+    }
+}
